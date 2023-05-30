c414a908e3889f2ecff1cdec7f5d7a94a8d4f805
MadeKarma <madekarma830@gmail.com>
2023-05-30 17:53:34 +0800

    Add event at navbar when scrolling change the background navbar

--- src/component/Navbar.tsx
+++ src/component/Navbar.tsx
@@ -1,23 +1,33 @@
+import { useState } from 'react'
 import icon from '../assets/img/iconbisloka.png'
 import { Link } from 'react-router-dom'
 
-
 const Navbar = () => {
+  const [Navbar, setNavbar] = useState(false)
+  window.addEventListener('scroll', () => {
+    // if (window.scrollY >= 20) {
+    //   setNavbar(true)
+    // } else {
+    //   setNavbar(false)
+    // }
+    window.scrollY? setNavbar(true) : setNavbar(false)
+  })
+
   return (
-    <header className='fixed top-0 w-full z-50 duration-200'>
+    <header className={`${Navbar? "bg-white shadow-lg" : "" } fixed top-0 w-full z-50 duration-200`}>
       <div className='flex items-center justify-between px-32 py-4'>
-        <nav className='flex font-semibold gap-8 text-slate-400'>
+        <nav className='flex font-normal gap-8 text-[#828282]'>
           <img src={icon} alt="" />
-          <Link to={'#'}>Booking</Link>
-          <Link to={'#'}>List Bus</Link>
-          <Link to={'#'}>Blog</Link>
-          <Link to={'#'}>Tentang Kami</Link>
+          <Link to={'#'} className='hover:text-[#222]'>Booking</Link>
+          <Link to={'#'} className='hover:text-[#222]'>List Bus</Link>
+          <Link to={'#'} className='hover:text-[#222]'>Blog</Link>
+          <Link to={'#'} className='hover:text-[#222]'>Tentang Kami</Link>
         </nav>
-        <div className='flex gap-4 font-semibold text-white buttonNav'>
-          <button className='px-6 py-2 rounded-lg'>
+        <div className={`flex gap-4 font-semibold ${Navbar? 'text-[#3BC5E9]' : 'text-white'} buttonNav`}>
+          <button className='duration-200 hover:text-[#333] px-6 py-2 rounded-lg transition-colors'>
             <span>Sign In</span>
           </button>
-          <button className='border-2 border-white px-6 py-2 rounded-lg' id='buttonSignUp'>
+          <button className={`border-2 ${Navbar? 'border-[#3BC5E9]' : 'border-white'} duration-200 hover:bg-slate-400 hover:border-slate-700 hover:text-[#333] px-6 py-2 rounded-lg transition-colors`}>
             <span>Sign Up</span>
           </button>
         </div>
